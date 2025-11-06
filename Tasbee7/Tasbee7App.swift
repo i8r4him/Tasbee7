b8b608bbcadf62f09e9e14c59fa3a70b678da043
i8r4him <konflikt.behoben0a@icloud.com>
2025-11-06 14:49:40 +0100

    Initial Commit

---
+++ Tasbee7/Tasbee7App.swift
@@ -0,0 +1,17 @@
+//
+//  Tasbee7App.swift
+//  Tasbee7
+//
+//  Created by Ibrahim Abdullah on 06.11.25.
+//
+
+import SwiftUI
+
+@main
+struct Tasbee7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
